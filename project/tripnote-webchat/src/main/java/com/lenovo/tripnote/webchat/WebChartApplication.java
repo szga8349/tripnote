8f45c0528c001ad89cd69f2867c70f9aa5bfa439
Jiyu JY2 Shi <shijy2@Lenovo.com>
2018-06-11 10:21:34 +0800

    '增加webchat'

--- project/tripnote-webchat/src/main/java/com/lenovo/tripnote/webchat/WebChartApplication.java
+++ project/tripnote-webchat/src/main/java/com/lenovo/tripnote/webchat/WebChartApplication.java
@@ -1,15 +1,8 @@
 package com.lenovo.tripnote.webchat;
 
-import org.apache.catalina.Context;
-import org.apache.catalina.connector.Connector;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
@@ -23,7 +16,7 @@ public class WebChartApplication {
     /**
      * it's for set http url auto change to https
      */
-    @Bean
+  /*  @Bean
     public EmbeddedServletContainerFactory servletContainer(){
         TomcatEmbeddedServletContainerFactory tomcat=new TomcatEmbeddedServletContainerFactory(){
             @Override
@@ -49,5 +42,5 @@ public class WebChartApplication {
         connector.setRedirectPort(8443);
         return connector;
     }
-
+*/
 }
