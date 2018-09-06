2f4f244e56af73608134b56c4f362640261a4aa1
dujz1 <dujz1@lenovo.com>
2018-09-06 16:49:40 +0800

    修改上传文件大小限制为5M

---
+++ project/tripnote-webchat/src/main/java/com/lenovo/tripnote/webchat/wx/api/TestAPI.java
@@ -0,0 +1,12 @@
+package com.lenovo.tripnote.webchat.wx.api;
+
+/**
+ * Created by dujz1 on 2018/8/27.
+ */
+public class TestAPI {
+	public static void main(String[] strings){
+		TestClass testClass = new TestClass();
+//		String a = testClass.aa();
+//		System.out.println(a);
+	}
+}
---
+++ project/tripnote-webchat/src/main/java/com/lenovo/tripnote/webchat/wx/api/TestClass.java
@@ -0,0 +1,10 @@
+package com.lenovo.tripnote.webchat.wx.api;
+
+/**
+ * Created by dujz1 on 2018/8/27.
+ */
+public class TestClass {
+	private static String aa(){
+		return "abc";
+	}
+}
